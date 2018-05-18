d9764a7600f2bc55ce7c7802f3a69c07caa7babc
Morten Lindhardt <r3Fuze@gmail.com>
2018-05-18 21:28:45 +0200

    Implement Google Analytics and fix syntax

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react"
+import ReactGA from "react-ga"
+import logo from "./logo.svg"
+import "./App.css"
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    constructor() {
+        super()
+
+        ReactGA.initialize("UA-18717141-5")
+        ReactGA.pageview(window.location.pathname)
+    }
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <h1 className="App-title">Welcome to React</h1>
+                </header>
+                <p className="App-intro">
+                    To get started, edit <code>src/App.js</code> and save to reload.
+                </p>
+            </div>
+        )
+    }
 }
 
-export default App;
+export default App
