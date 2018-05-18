640f3be43bf50dfea44de76d9dce2a483366eb21
Morten Lindhardt <r3Fuze@gmail.com>
2018-05-19 01:37:33 +0200

    Logo work

--- src/App.js
+++ src/App.js
@@ -7,8 +7,9 @@ class App extends Component {
     constructor() {
         super()
 
-        ReactGA.initialize("UA-18717141-5")
-        ReactGA.pageview(window.location.pathname)
+        ReactGA.initialize("UA-18717141-5", { 
+            testMode: process.env.NODE_ENV === "test",
+        })
     }
     render() {
         return (
@@ -20,6 +21,7 @@ class App extends Component {
                 <p className="App-intro">
                     To get started, edit <code>src/App.js</code> and save to reload.
                 </p>
+                <input type="button" value="Test" onClick={ReactGA.event({category:"User",action:"Pressed the button"})}/>
             </div>
         )
     }
