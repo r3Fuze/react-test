8a64acbf974cc1fba4e575730dd013827e8ce0f6
Morten Lindhardt <r3Fuze@gmail.com>
2018-05-20 18:56:20 +0200

    feat(style): Start implementing material-ui

--- src/App.js
+++ src/App.js
@@ -3,6 +3,18 @@ import ReactGA from "react-ga"
 import logo from "./logo.svg"
 import "./App.css"
 
+import Button from "@material-ui/core/Button"
+
+const style = {
+    background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)",
+    borderRadius: 3,
+    border: 0,
+    color: "white",
+    height: 48,
+    padding: "0 30px",
+    boxShadow: "0 3px 5px 2px rgba(255, 105, 135, 0.3)"
+}
+
 class App extends Component {
     constructor() {
         super()
@@ -21,7 +33,13 @@ class App extends Component {
                 <p className="App-intro">
                     To get started, edit <code>src/App.js</code> and save to reload.
                 </p>
-                <input type="button" value="Test" onClick={ReactGA.event({category:"User",action:"Pressed the button"})}/>
+                
+                <Button
+                    style={style}
+                    onClick={ReactGA.event({category:"User",action:"Pressed the button"})}
+                >
+                    Test button
+                </Button>
             </div>
         )
     }
