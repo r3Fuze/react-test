065430a6d862807d5626105668672653cd5dd63e
Morten Lindhardt <r3Fuze@gmail.com>
2018-05-24 01:34:22 +0200

    refactor: remove some exclamation points

--- src/App.js
+++ src/App.js
@@ -34,7 +34,7 @@ class App extends Component {
                     <h1 className="App-title">Welcome to React (now with GA)</h1>
                 </header>
                 <p className="App-intro">
-                    To get started, edit <code>src/App.js</code> and save to reload!!!!
+                    To get started, edit <code>src/App.js</code> and save to reload!
                 </p>
 
                 <Button
