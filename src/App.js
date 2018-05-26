3d2bb9dbf29953b5bb529cde2ebef307d691aab4
Morten Lindhardt <r3Fuze@gmail.com>
2018-05-26 22:02:20 +0200

    refactor: switch from jss injection to && { ... } syntax for now

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import TestButton from "components/TestButton"
 
 const ga = window.ga
 
-const StyledButton = styled(Button)`
+const StyledButton = styled(Button)`&& {
     background: linear-gradient(45deg, #fe6b8b 30%, #ff8e53 90%);
     border-radius: 3px;
     border: 0;
@@ -17,7 +17,7 @@ const StyledButton = styled(Button)`
     height: 48px;
     padding: 0 30px;
     box-shadow: 0 3px 5px 2px rgba(255, 105, 135, 0.3);
-`
+}`
 
 class App extends Component {
     render() {
--- src/index.js
+++ src/index.js
@@ -8,30 +8,5 @@ import "material-icons"
 import App from "./App"
 import registerServiceWorker from "./registerServiceWorker"
 
-// TODO: See https://material-ui.com/guides/interoperability/#controlling-priority
-//           https://material-ui.com/customization/css-in-js/#css-injection-order
-// Required to have styled-components styles overwrite material-ui
-// Alternative is surrounding styled-components css with && { ... }
-// This may break something in tests if we just import <App/> we might have to move it to App.js
-import { create } from "jss"
-import JssProvider from "react-jss/lib/JssProvider"
-import { createGenerateClassName, jssPreset } from "@material-ui/core/styles"
-
-const styleNode = document.createComment("insertion-point-jss")
-document.head.insertBefore(styleNode, document.head.firstChild)
-
-const jss = create(jssPreset())
-jss.options.insertionPoint = "insertion-point-jss"
-
-class JssApp extends React.Component {
-    render() {
-        return (
-            <JssProvider jss={jss} generateClassName={createGenerateClassName()}>
-                <App/>
-            </JssProvider>
-        )
-    }
-}
-
-ReactDOM.render(<JssApp/>, document.getElementById("root"))
+ReactDOM.render(<App/>, document.getElementById("root"))
 registerServiceWorker()
