adf702ffc23c4fe8a6dd91f5b4bb2cd2d2fcd315
Morten Lindhardt <r3Fuze@gmail.com>
2018-05-26 17:08:34 +0200

    refactor: use material-icons from npm instead of google

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from "react"
 import ReactDOM from "react-dom"
+
 import "./index.css"
 import "typeface-roboto"
+import "material-icons"
+
 import App from "./App"
 import registerServiceWorker from "./registerServiceWorker"
 
