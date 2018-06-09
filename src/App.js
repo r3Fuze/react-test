5c2f059f5a3ec32e88567bda61d4ac5a456e7f25
Morten Lindhardt <r3Fuze@gmail.com>
2018-06-09 18:40:46 +0200

    chore: disable analytics for now

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Button from "@material-ui/core/Button"
 
 import TestButton from "components/TestButton"
 
-const ga = window.ga
+const ga = () => { } // window.ga
 
 const StyledButton = styled(Button)`&& {
     background: linear-gradient(45deg, #fe6b8b 30%, #ff8e53 90%);
